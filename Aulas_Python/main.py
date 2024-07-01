76dbcb6388c3155e457921f0679d03813259c209
Juliezer da Silva <juliezersilva.contato@gmail.com>
2024-07-01 15:34:33 +0000

    Aula 01

---
+++ Aulas_Python/main.py
@@ -0,0 +1,12 @@
+#Programa para soma de números:
+a = input(
+    'Digite o primeiro número: '
+)
+b = input(
+    'Digite o segundo número: '
+)
+print(
+    'A soma dos números é:',
+    int(a) + int(b)
+)
+#Fim do programa
