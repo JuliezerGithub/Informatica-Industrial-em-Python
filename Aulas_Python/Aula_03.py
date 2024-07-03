614c7ce5de091637220f8663c66131348c7fdcfe
Juliezer da Silva <juliezersilva.contato@gmail.com>
2024-07-03 15:32:57 +0000

    Complemento Aula 03

--- Aulas_Python/Aula_03.py
+++ Aulas_Python/Aula_03.py
@@ -4,4 +4,21 @@
 lista = [1, 2, "Juliezer", 5.5, True, 'a'] # Fracamente tipada
 
 for x in lista:
-    print()
+    print(x)
+
+lista.append("Soares")
+lista[0] = 55
+print('\n')
+for x in range(0,len(lista)):
+    if x == 2:
+        continue # Vai pular a posição na lista
+    print(lista[x])
+
+# Dicionários
+dicionario = {"curso":"informatica industrial", "número":4}
+print(dicionario)
+
+#Mudar elemento do dicionario
+
+dicionario["curso"] = "Robótica Móvel"
+print(dicionario)
