ebbc61140d84e4ebf16a401d34f12577e8a3b1ca
Juliezer da Silva <juliezersilva.contato@gmail.com>
2024-07-04 15:31:54 +0000

    Aula 05 em andamento

--- Aulas_Python/Aula_04.py
+++ Aulas_Python/Aula_04.py
@@ -1,7 +1,6 @@
 # Aula 4 - Introdução ao Python: Variáveis e Objetos
 
-# Objetos Imutáveis
-
+# Objetos Imutáveis -  Recebem referencias diferentes
 #nome (variável) | objeto
 
 a = 3
@@ -23,3 +22,13 @@ a = "informatica"
 print("Valor: ", a)
 print("Identificador a: ", id(a))
 
+# Objetos Mutáveis - Recebem a mesma referência
+#nome (variável) | objeto
+
+a =  [1,2,3]
+b = a
+b.append(4)
+
+print("Valor: ", a)
+print("Identificador a: ", id(a))
+print("Identificador b: ", id(b))
---
+++ Aulas_Python/Aula_05.py
@@ -0,0 +1,10 @@
+# Aula 05 - Introdução ao Python: Sub-rotinas e Módulos
+
+def soma(op1, op2):
+    """
+    Função que retorna a soma de dois números
+    :param op1: primeiro operando
+    :param op2: segundo operando
+    :return   : soma dos operandos
+    """
+    
